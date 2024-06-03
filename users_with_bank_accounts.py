38f7e42839d9d6dd943e8967f6c4457deb2d2fa7
Reem Joudeh <joudehreem@gmail.com>
2024-06-03 17:31:57 +0300

    final submit

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -1,57 +1,44 @@
-class BankAccount:
-  
-  def __init__(self, int_rate, balance):
-    self.int_rate=int_rate
-    self.balance=balance
-  
-  def deposit(self, amount):
-    self.balance+=amount
-    return self
-  def withdraw(self, amount):
-    if self.balance>amount:
-      self.balance-=amount
-    else:
-      print("Insufficient funds: Charging a $5 fee")
-      self.balance-=5
-    return self
-  def display_account_info(self):
-    print(f"Balance: ${self.balance}")
-    return self
-  def yield_interest(self):
-    if self.balance > 0:
-      self.balance += self.balance * self.int_rate
-    return self
-  
-
+from bankaccount import BankAccount
 class User:
-    def __init__(self,name,accounts):
+    def __init__(self,name):
       self.name=name
-      self.accounts={}
-      
-    def add_account(self, account_name, int_rate, balance):
-      self.accounts[account_name] = BankAccount(int_rate, balance)
-      return self
+      self.account=BankAccount(14,5000)
+    def create_account(self,account_type):
+      self.account_type=account_type
     def make_deposit (self, amount):
-      self.balance+=amount
-      print(f"user:{self.name} ,Deposit:${amount} ,Balance:${self.balance}")
+      self.account.deposit(amount)
+      print(f"user:{self.name} ,Deposit:${amount} ,Balance:${self.account.balance}")
       return self
-    
     def make_withdrawal(self, amount):
-      self.balance-=amount
-      print(f"user:{self.name} ,withdrawal:${amount} ,Balance:${self.balance}")
+      self.account.withdraw(amount)
+      print(f"user:{self.name} ,withdrawal:${amount} ,Balance:${self.account.balance}")
       return self
-    
     def display_user_balance(self):
-      print(f"user:{self.name} ,Balance:${self.balance}")
+      print(f"user:{self.name} ,Balance:${self.account.balance}")
       return self
-    
     def transfer_money(self, other_user, amount):
-      self.other_user=other_user
-      self.amount=amount
-      if amount<=self.balance:
-        self.balance-=amount
-        other_user.balance+=amount
-        print(f"Transfer to user:{other_user.name} ,Balance:${other_user.balance} My account {self.name} ,Balance:${self.balance}" )
+      if amount<=self.account.balance:
+        self.account.withdraw(amount)
+        other_user.account.deposit(amount)
+        print(f"Transfer to user:{other_user.name} ,Balance:${other_user.balance} My account {self.name} ,Balance:${self.account.balance}" )
       else:
-        print(f"Available balance: ${self.balance}")
+        print(f"Available balance: ${self.account.balance}")
       return self
+    def yield_interest(self):
+      self.account.yield_interest()
+      return self
+user1=User("Reem")
+user1.make_deposit(20)\
+.make_deposit(50)\
+.make_deposit(1000)\
+.make_withdrawal(500)\
+.yield_interest()\
+.display_user_balance()
+
+user2=User("jad")
+user2.make_deposit(20000)\
+.make_deposit(500)\
+.make_deposit(100000)\
+.make_withdrawal(500)\
+.yield_interest()\
+.display_user_balance()
